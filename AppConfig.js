08d15c815d86af08ad75b026d153943fc4aff639
navishshanbog <navish.shanbog@gmail.com>
2019-01-19 01:39:20 +1100

    irc
    irc

---
+++ AppConfig.js
@@ -0,0 +1,9 @@
+window.AppConfig = {
+	"production": false,
+	"basePath": "/analystdesktop/widget/site/",
+	"fabricIconBasePath": "/analystdesktop/widget/site/icons/fabric/",
+	"fabricFontBasePath": "/analystdesktop/widget/site",
+	"buildVersion": "DEV",
+	"buildDate": "2019-01-18T14:34:21.263Z",
+	"env": {}
+};
